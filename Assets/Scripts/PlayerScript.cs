c26f65552de0a20ac301498fb43e0ac208c08cad
Daniel Souza <danieldomingosdev@gmail.com>
2022-03-31 02:27:40 -0300

    Player
    +Colision
    +Death Condition
    *Developer Ui

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,50 +3,137 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PlayerScript : MonoBehaviour
 {
+    //Variaveis Input Pc
+    PlayerPos currentPos;
+     public enum PlayerPos {Left, Right, Middle};
+    //Variaveis Input Mobile
     private Vector2 startTouchPosition;
     private Vector2 currentPosition;
     private Vector2 endTouchPositon;
     private bool stopTouch = false;
     public float swipeRange;
     public float tapRange;
-    
-    
+    //Stats
+    private Vector3 jump;
+    private Rigidbody rb;
     public float jumpForce;
-    public Rigidbody player;
+    public bool firstHit;
     void Start()
     {
-        
+        //Starta Variaveis Stats/Posiçao Inicial
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+        currentPos = PlayerPos.Middle;
+    }
+    //Check Bool isGround
+    public bool isGrounded()
+    {
+        if (transform.position.y <= 0.01f)
+            return true;
+        else
+            return false;
     }
     void Update()
     {
-        //InputsPC
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            if (transform.position.x == -0.5f)
-            {
-                return;
-            }else
-                transform.Translate(-0.5f,0,0);
-        }else if(Input.GetKeyDown(KeyCode.D))
-        {
-            if (transform.position.x == 0.5f)
-            {
-                return;
-            }else
-                transform.Translate(0.5f,0,0);
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        { 
-            player.AddForce(0,jumpForce,0);
-        }
+        //Chama Inputs PC
+        InputsPc();
+        
         //InputsMobile
+        //Revisar Mobile*
+        MobileInput();
+        
+        //Pulo &&check Ground
+                if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
+                {
+                    rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+                }
+    }
+    //Colisão
+    private void OnCollisionEnter(Collision col)
+    {
+        //Var Posiçao apos HIT
+        float hitPos = 0;
+        if (col.gameObject.CompareTag("Obstacle"))
+        {
+            //TestaCondiçao Derrota
+            if (firstHit == true)
+            {
+                //Chamar condiçao Derrota
+                Debug.Log("Derrota");
+            }
+            //Seta Condiçoes 'First Hit'
+            firstHit = true;
+            hitPos = -.7f;
+            //Executa FirstHit
+            if (currentPos == PlayerPos.Left)
+            {
+                this.transform.position = new Vector3(0, transform.position.y, hitPos);
+                currentPos = PlayerPos.Middle;
+            }else if (currentPos == PlayerPos.Right)
+            {
+                this.transform.position = new Vector3(0, transform.position.y, hitPos);
+                currentPos = PlayerPos.Middle;
+            }else if (currentPos == PlayerPos.Middle)
+            {
+                int i;
+                i = Random.Range(1, 3);
+                if (i == 1)
+                {
+                    this.transform.position = new Vector3(-.5f, transform.position.y, hitPos);
+                    currentPos = PlayerPos.Left;
+                }else if (i == 2)
+                {
+                    this.transform.position = new Vector3(.5f, transform.position.y,hitPos);
+                    currentPos = PlayerPos.Right;
+                }
+            }
+        }
+    }
+
+    //InputsPC
+    private void InputsPc()
+    {
+        if (Input.GetKeyDown(KeyCode.A) && currentPos != PlayerPos.Left)
+        {
+            if (currentPos == PlayerPos.Right)
+            {
+                this.transform.position = new Vector3(0, transform.position.y, transform.position.z);
+                currentPos = PlayerPos.Middle;
+            }
+            else
+            {
+                this.transform.position = new Vector3(-.5f, transform.position.y, transform.position.z);
+                currentPos = PlayerPos.Left;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.D) && currentPos != PlayerPos.Right)
+        {
+            if (currentPos == PlayerPos.Left)
+            {
+                this.transform.position = new Vector3(0, transform.position.y, transform.position.z);
+                currentPos = PlayerPos.Middle;
+            }
+            else
+            {
+                this.transform.position = new Vector3(.5f, transform.position.y, transform.position.z);
+                currentPos = PlayerPos.Right;
+            }
+        }
+    }
+
+    //Revisar Mobile*
+    private void MobileInput()
+    {
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
             startTouchPosition = Input.GetTouch(0).position;
         }
+
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
         {
             currentPosition = Input.GetTouch(0).position;
@@ -61,8 +148,9 @@ public class PlayerScript : MonoBehaviour
                     if (transform.position.x == -0.5f)
                     {
                         return;
-                    }else
-                        transform.Translate(-0.5f,0,0);
+                    }
+                    else
+                        transform.Translate(-0.5f, 0, 0);
                 }
                 else if (Distance.x > swipeRange)
                 {
@@ -72,15 +160,18 @@ public class PlayerScript : MonoBehaviour
                     if (transform.position.x == 0.5f)
                     {
                         return;
-                    }else
-                        transform.Translate(0.5f,0,0);
-                }else if (Distance.y > swipeRange)
+                    }
+                    else
+                        transform.Translate(0.5f, 0, 0);
+                }
+                else if (Distance.y > swipeRange)
                 {
                     //Jump
                     stopTouch = true;
                     Console.WriteLine("Jump");
-                    player.AddForce(0,jumpForce,0);
-                }else if (Distance.y > -swipeRange)
+                    rb.AddForce(0, jumpForce, 0);
+                }
+                else if (Distance.y > -swipeRange)
                 {
                     //Slide
                     stopTouch = true;
@@ -88,12 +179,12 @@ public class PlayerScript : MonoBehaviour
                 }
             }
         }
+
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
         {
             stopTouch = false;
             endTouchPositon = Input.GetTouch(0).position;
             Vector2 Distance = endTouchPositon - startTouchPosition;
-            
         }
     }
 }
